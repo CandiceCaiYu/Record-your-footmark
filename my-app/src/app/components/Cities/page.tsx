30a025cdd6c429aa74e3908935b178d856999d62
CandiceCaiYu <6caiyu6@gmail.com>
2023-12-19 15:58:10 +0800

    [Trello-28] save form data in city page

--- my-app/src/app/components/Cities/page.tsx
+++ my-app/src/app/components/Cities/page.tsx
@@ -17,9 +17,9 @@ interface Props {
 const Cities = ({provinceCode, cleanProvinceCode}: Props) => {
     const [cityChart, setCityChart] = useState<echarts.ECharts>()
     const {cityTravelInfo} = usePage(provinceCode, cityChart)
-    const [currentCityInfo, setCurrentCityInfo] = useState<City_info>(cityTravelInfo?.[0] || {})
+    const [currentCityInfo, setCurrentCityInfo] = useState<City_info>()
 
-    const handleOnChange = (date: Dayjs | null, dateString: string) => {
+    const handleDateChange = (date: Dayjs | null, dateString: string) => {
         setCurrentCityInfo({
             ...currentCityInfo,
             date: date || undefined
@@ -33,6 +33,16 @@ const Cities = ({provinceCode, cleanProvinceCode}: Props) => {
         })
     }
 
+    const handleSubmit = () => {
+        console.log('cr...', currentCityInfo)
+    }
+
+    useEffect(() => {
+        setCurrentCityInfo({
+            ...currentCityInfo,
+            ...(cityTravelInfo?.[0] || {}),
+        })
+    }, [cityTravelInfo]);
     useEffect(() => {
         const myChart = echarts.init(document.getElementById('city'));
         setCityChart(myChart)
@@ -43,21 +53,21 @@ const Cities = ({provinceCode, cleanProvinceCode}: Props) => {
         <div className={styles.city_wrapper}>
             <div id={'city'} style={{width: '1200px', height: '1080px'}}></div>
             <section className={styles.city_content}>
-                <h2>{currentCityInfo.provinceName}{currentCityInfo.cityName && `- ${currentCityInfo.cityName}}`}</h2>
+                <h2>{currentCityInfo?.provinceName}{currentCityInfo?.cityName && `- ${currentCityInfo.cityName}}`}</h2>
                 <Form labelCol={{span: 3}} wrapperCol={{span: 16}} size={"large"}>
                     <Form.Item label={'出发时间'} name={'date'} wrapperCol={{span: 6}}>
                         <DatePicker defaultValue={currentCityInfo?.date}
-                                    onChange={handleOnChange}
+                                    onChange={handleDateChange}
                                     className={styles.datePicker}/>
                     </Form.Item>
                     <Form.Item label={'内容'} name={'content'} wrapperCol={{span: 20}}>
-                        <MDEditor value={currentCityInfo.content} onChange={handleContentChange}></MDEditor>
+                        <MDEditor value={currentCityInfo?.content} onChange={handleContentChange}></MDEditor>
                         {/*<MDEditor.Markdown source={currentCityInfo.content}></MDEditor.Markdown>*/}
                     </Form.Item>
                     <Form.Item>
                         <Space>
-                            <Button>Cancel</Button>
-                            <Button type="primary" htmlType={'button'}>Save</Button>
+                            <Button htmlType={'reset'}>Cancel</Button>
+                            <Button type="primary" htmlType={'submit'} onClick={handleSubmit}>Submit</Button>
                         </Space>
                     </Form.Item>
                 </Form>
